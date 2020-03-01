2601a5e4c4703fe92336630a4038b0f11623535d
Rznec <gamazrz@gmail.com>
2020-03-01 20:30:06 +0300

    Coordinates of field cells are added

--- src/game/FieldBattle.java
+++ src/game/FieldBattle.java
@@ -4,6 +4,7 @@ package game;
  * Игровое поле.
  */
 public class FieldBattle {
+    private char[] Hfield;
     private String[][] field;
     private int sizeField = 10;
 
@@ -28,11 +29,26 @@ public class FieldBattle {
      * Вывод поля в консоль.
      */
     public void printField() {
+        Hfield = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+        int coordX = 1;
+        int coordY = 0;
+        System.out.print(" ");
         for (int x = 0; x < sizeField; x++) {
-            for (int y = 0; y < sizeField; y++) {
+            System.out.print(" ");
+            System.out.print(coordX++);
+        }
+        for (int y = 0; y < sizeField; y++) {
+            if (!(y == 0 && coordX == 11)) ;
+        }
+        System.out.println();
+        for (int y = 0; y < sizeField; y++) {
+            System.out.print(Hfield[coordY++]);
+            for (int x = 0; x < sizeField; x++) {
+                if (!(x == 0 && coordY == 11))
+                    System.out.print(" ");
                 System.out.print(field[x][y]);
             }
-            System.out.println("");
+            System.out.println(" ");
         }
     }
 }
